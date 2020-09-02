31e7db2ee30b07a20192a0a5cdf05c2d44c65384
Chaz Sherrill <chazsherrill@icloud.com>
2020-09-02 13:17:37 -0500

    Here's my guessing game

---
+++ NumberGuessingGame/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace NumberGuessingGame// this is a project i found on youtube https://www.youtube.com/watch?v=IhqdPDPV_g8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random r = new Random();
+
+            int winningNumber = r.Next(1, 100);
+
+            bool win = false;
+
+            do
+            {
+                Console.Write("guess a number between 1 and 100.            ");
+                string yourGuess = Console.ReadLine();
+
+                int guessNumber = int.Parse(yourGuess);
+
+                if (guessNumber > winningNumber)
+                {
+                    Console.WriteLine("Too high...Guess lower");
+                }
+                else if(guessNumber < winningNumber)
+                {
+                    Console.WriteLine("Too low....guess higher");
+                }
+                else if ( guessNumber == winningNumber)
+                {
+                    Console.WriteLine("Great Job! You win!");
+                    win = true;
+                }
+
+                Console.WriteLine("");
+            } while (win == false);
+
+            Console.WriteLine("Thank you for playing!");
+            Console.WriteLine("Press any key to finish");
+            Console.ReadKey(true);
+        }
+    }
+}
